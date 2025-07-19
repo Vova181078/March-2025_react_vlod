0c48fd0cbca3e475195ffaf4ae3a86bed95014a2
Volodymyr <volodymyrua78@gmail.com>
2025-07-19 23:00:11 +0200

    Base Commit; Homeworks; less-1_task1/courseTitleArray;

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import './App.css'
+import {coursesTitleArray} from "./data/coursesTitleArray.ts";
 
 function App() {
 
 
   return (
     <>
+     <div>
+       {
+      coursesTitleArray.map((course,index) => <p key={index}>{course}</p>)
 
-    </>
+       }
+     </div>
+   </>
   )
 }
 
---
+++ src/data/coursesTitleArray.ts
@@ -0,0 +1,10 @@
+
+export const coursesTitleArray:string[] = [
+    'JavaScript Complex',
+    'Java Complex',
+    'Python Complex',
+    'QA Complex',
+    'Fullstack',
+    'Frontend'
+];
+
